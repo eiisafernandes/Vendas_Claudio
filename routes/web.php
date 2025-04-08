32c8dc0525f69247b2c7a9f4cca89c099ac2ed7d
eiisafernandes <isaafernandes2018@gmail.com>
2025-04-08 13:08:03 -0300

    Atualização - TerçaClaudio

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Admin\Dashboard;
+use App\Livewire\Admin\Funcionario\FuncionarioIndex;
 use App\Livewire\Auth\Login;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', Login::class)->name('login');
 
 Route::get('admin/dashboard', Dashboard::class)->middleware(['auth', 'user_type:admin'])->name('admin.dashboard');
+
+Route::get('admin/funcionario', FuncionarioIndex::class)->middleware('auth', 'user_type:admin')->name('admin.funcionario.index');
